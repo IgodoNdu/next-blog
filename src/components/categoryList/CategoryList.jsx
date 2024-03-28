73070daa0b814513f47c6fea544d95a99e1fac3d
IgodoNdu <darkbluetechmail@gmail.com>
2024-03-29 00:12:09 +0100

    Creating the components files

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./categoryList.module.css"
+
+const CategoryList = () => {
+  return (
+    <div className={StyleSheet.container}>
+        CategoryList
+    </div>
+  )
+}
+
+export default CategoryList
